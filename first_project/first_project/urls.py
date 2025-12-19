30bcdedfa65f73207185b505be3121f75a676a7a
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2025-12-19 20:36:21 +0200

    Update urls.py

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -1,31 +1,7 @@
-"""
-URL configuration for first_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from main.views import (
-    current_datetime,
-    multiplication_table,
-    programmers_day
-)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('datetime/', current_datetime),
-    path('table/', multiplication_table),
-    path('programmer-day/', programmers_day),
+    path('', include('hw_11_12.urls')),
 ]
-
