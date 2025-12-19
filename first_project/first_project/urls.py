be0ad92400f6da37b4449a383e9eef1ccd739413
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2025-12-19 20:49:19 +0200

    Update urls.py

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hw_11_12.urls')),
+
+    path('', include('hw_13_12.urls')),        
+    path('city/', include('hw_11_12.urls')),  
 ]
