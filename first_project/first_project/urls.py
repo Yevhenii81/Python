2e852bac32de9fa332d2c288eb9cd818374648b0
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2025-12-25 15:31:16 +0200

    Add library path to urlpatterns

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -4,7 +4,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('hw_16_12.urls')),  
-    path('old/', include('hw_13_12.urls')), 
-    path('city/', include('hw_11_12.urls')),  
+    path('', include('hw_16_12.urls')),        
+    path('old/', include('hw_13_12.urls')),    
+    path('city/', include('hw_11_12.urls')),   
+
+    path('library/', include('hw_18_12.urls')),  
 ]
