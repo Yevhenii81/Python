52ea49cf4bfd6a61503bf16a2fd6ccaf9536108c
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2026-01-04 15:21:19 +0200

    Update URL patterns to include hw20 and hw16

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -4,9 +4,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('hw_16_12.urls')),        
-    path('old/', include('hw_13_12.urls')),    
-    path('city/', include('hw_11_12.urls')),   
+    path('', include('hw_20_12.urls')),
 
-    path('library/', include('hw_18_12.urls')),  
+    path('old/', include('hw_13_12.urls')),
+    path('city/', include('hw_11_12.urls')),
+    path('library/', include('hw_18_12.urls')),
+    path('hw16/', include('hw_16_12.urls')),
 ]
