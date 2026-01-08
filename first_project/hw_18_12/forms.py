d5adc2aa21b2cdb301499b11d354e013bd85907f
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2026-01-08 15:35:47 +0200

    Add files via upload

---
+++ first_project/hw_18_12/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Book, Reader
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = [
+            'title',
+            'author',
+            'year',
+            'style',
+            'publisher',
+            'is_available'
+        ]
+
+
+class ReaderForm(forms.ModelForm):
+    class Meta:
+        model = Reader
+        fields = [
+            'first_name',
+            'last_name',
+            'phone',
+            'email',
+            'register_date'
+        ]
