0b6142ad43f7609a153b97998f1f625f35a9a44b
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2026-01-08 15:36:23 +0200

    Enhance URL patterns for books and readers
    Added new URL patterns for book and reader management.

--- first_project/hw_18_12/urls.py
+++ first_project/hw_18_12/urls.py
@@ -2,11 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.library_home),
-    
-    path('books/', views.book_list),
-    path('books/<int:book_id>/', views.book_detail),
+    path('', views.library_home, name='library_home'),
 
-    path('readers/', views.reader_list),
-    path('readers/<int:reader_id>/', views.reader_detail),
+    path('books/', views.book_list, name='book_list'),
+    path('books/available/', views.book_available_list, name='book_available'),
+    path('books/add/', views.book_create, name='book_create'),
+    path('books/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('books/<int:book_id>/edit/', views.book_update, name='book_update'),
+    path('books/<int:book_id>/delete/', views.book_delete, name='book_delete'),
+
+    path('readers/', views.reader_list, name='reader_list'),
+    path('readers/add/', views.reader_create, name='reader_create'),
+    path('readers/<int:reader_id>/', views.reader_detail, name='reader_detail'),
+    path('readers/<int:reader_id>/edit/', views.reader_update, name='reader_update'),
+    path('readers/<int:reader_id>/delete/', views.reader_delete, name='reader_delete'),
 ]
